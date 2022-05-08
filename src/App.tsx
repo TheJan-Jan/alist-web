de5cf4927860e9e1d5e05a44e240df13cc8292ac
TheJan-Jan <62783500+TheJan-Jan@users.noreply.github.com>
2022-05-08 19:49:15 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
           }
         >
           <Switch>
-            <Route path="/@manage/">
+            <Route path="/@TheJan">
               <Manage />
             </Route>
             <Route path="*">
